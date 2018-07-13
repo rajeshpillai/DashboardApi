d28f79ecfeff9e419325c622bd18c2f40c22cea3
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-07-13 12:57:34 +0530

    changes done to query engine to select correct tables in query

--- DashboardApi/Controllers/DataController.cs
+++ DashboardApi/Controllers/DataController.cs
@@ -18,6 +18,10 @@ namespace DashboardApi.Controllers
     {
         static object PageData = null;
 
+        Dashboard dashboard = null;
+
+        
+
         // GET: api/Data       
         [HttpGet]
         public IEnumerable<dynamic> Get()
@@ -39,13 +43,141 @@ namespace DashboardApi.Controllers
             return employees;
         }
 
+        private void BuildTableMetatdata()
+        {
+            dashboard = new Dashboard();            
+            dashboard.AddTable(new Table() { Name = "Product" });
+            dashboard.AddTable(new Table() { Name = "ProductInventory" });
+            dashboard.AddTable(new Table() { Name = "ProductVendor" });
+            dashboard.AddTable(new Table() { Name = "PurchaseOrderDetail" });
+            dashboard.AddTable(new Table() { Name = "PurchaseOrderHeader" });
+
+            dashboard.Associations = GetAllTableAssociations();
+
+            var tablesCount = dashboard.Tables.Count();
+
+            int[,] graph = new int[tablesCount, tablesCount];
+
+            for (var u = 0; u < tablesCount; u++)
+            {
+                for (var v = 0; v < tablesCount; v++)
+                {
+                    //graph[u, v] = IsEdgeExist(u, v, associations, tables);
+                    graph[u, v] = IsEdgeExist(u, v, dashboard.Associations, dashboard);
+                }
+            }
+            var combinations = GetAllCombinations(dashboard);
+
+            //Get tables involed for each combination
+            var tablesInvolvedForCombi = new Dictionary<string, List<string>>();
+            foreach (var combi in combinations)
+            {
+                var tablesInvoled = combi.Split(',');
+                if (tablesInvoled.Length == 1)
+                {
+                    continue;
+                }
+                var tInvolvedForCombi = new List<string>();
+
+                for (int k = 0; k < tablesInvoled.Count() - 1; k++)
+                {
+                    var t = DashboardApi.Utility.Dijkstra.DijkstraAlgo(graph, Convert.ToInt32(tablesInvoled[k]), Convert.ToInt32(tablesInvoled[k + 1]), tablesCount);
+                    foreach (var tIndex in t.Split(','))
+                    {
+                        tInvolvedForCombi.Add(tIndex);
+                        //if (tInvolvedForCombi.Contains(tIndex))
+                        //{
+                        //    tInvolvedForCombi.Remove(tIndex);
+                        //    tInvolvedForCombi.Add(tIndex);
+                        //}
+                        //else
+                        //{
+                        //    tInvolvedForCombi.Add(tIndex);
+                        //}
+                    }
+                }
+                if (!tInvolvedForCombi.Contains(tablesInvoled[tablesInvoled.Count() - 1]))
+                {
+                    tInvolvedForCombi.Add((tablesInvoled[tablesInvoled.Count() - 1]).ToString());  //Insert last table if not exist in list
+                }
+
+
+                tablesInvolvedForCombi.Add(combi, tInvolvedForCombi);
+            }
+
+
+            var TableAssociationHash = new Dictionary<string, DerivedAssociation>();
+            foreach (var combi in combinations)
+            {
+                var da = new DerivedAssociation();
+
+                if (!tablesInvolvedForCombi.ContainsKey(combi))
+                {
+                    continue;
+                }
+                var tablesInvoled = tablesInvolvedForCombi[combi];//combi.Split(',');  //Get Tables involved for that combination
+                if (tablesInvoled.Count() == 1)
+                {
+                    continue;
+                }
+                //
+
+                //List<DerivedAssociation> dAssociations = new List<DerivedAssociation>();
+                var derivedAssociation = new DerivedAssociation();
+                var tablesConsidered = new List<string>();
+
+                derivedAssociation.TableName1 = dashboard.Tables.Where(tbl => tbl.Id == Convert.ToInt32(tablesInvoled[0])).First().Name;
+                tablesConsidered.Add(tablesInvoled[0]);
+                for (int j = 1; j < tablesInvoled.Count(); j++)//s (var t in tablesInvoled)
+                {
+                    //var curTableName = tables[Convert.ToInt32(tablesInvoled[j])];
+                    //var prevTableName = tables[Convert.ToInt32(tablesInvoled[j - 1])];
+
+                    var curTableName = dashboard.Tables.Where(tbl => tbl.Id == Convert.ToInt16(tablesInvoled[j])).First().Name;
+                    if (tablesConsidered.Contains(tablesInvoled[j]))
+                    {
+                        continue;
+                    }
+                    var prevTableName = dashboard.Tables.Where(tbl => tbl.Id == Convert.ToInt32(tablesInvoled[j - 1])).First().Name;
+
+                    var tableAssociation = dashboard.Associations.Where(a => a.TableName == prevTableName && a.Relations.Where(r => r.TableName2 == curTableName).FirstOrDefault() != null).FirstOrDefault();
+
+                    if (null != tableAssociation)
+                    {
+                        var relation = tableAssociation.Relations.Where(r => r.TableName2 == curTableName).FirstOrDefault();
+                        derivedAssociation.Relations.Add(relation);
+
+                        //dAssociations.Add(derivedAssociation);
+                    }
+                    tablesConsidered.Add(tablesInvoled[j]);
+
+                    //var tableAssociation= associations.Where(a => a.TableName == tables[Convert.ToInt32(tablesInvoled[j - 1])] && a.Relations.Where(r => r.TableName2 == tables[Convert.ToInt32(tablesInvoled[j])]).FirstOrDefault() != null ||
+                    //                     (a.TableName == tables[Convert.ToInt32(tablesInvoled[j])] && a.Relations.Where(r => r.TableName2 == tables[Convert.ToInt32(tablesInvoled[j - 1])]).FirstOrDefault() != null)).FirstOrDefault();
+
+                    //if (null != tableAssociation)
+                    //{
+                    //    derivedAssociation.Relations.Add(tableAssociation.Relations.Where(r=>r.TableName2 == tables[Convert.ToInt32(tablesInvoled[j - 1])] || r.TableName2 == tables[Convert.ToInt32(tablesInvoled[j])]).FirstOrDefault());
+
+                    //    dAssociations.Add(derivedAssociation);
+                    //}
+
+
+                }
+                TableAssociationHash.Add(combi, derivedAssociation);
+
+            }
+
+            dashboard.TableAssociationHash = TableAssociationHash;
+
+        }
+
         // GET: api/Data
         [Route("api/data/getall")]
         [HttpGet]
         [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
         public IEnumerable<dynamic> GetAll()
         {
-
+           
             var connection = new MySqlConnection(
              "Host=localhost;Port=3306;User=root;Password=root123;Database=adventureworks;SslMode=None"
             );
@@ -141,71 +273,76 @@ namespace DashboardApi.Controllers
             var db = new QueryFactory(connection, new MySqlCompiler());
             Query query = null;
 
-            var tables = GetTablesInvolved(widgetModel);
+            //var tables = GetTablesInvolved(widgetModel);
 
-            if (null != tables && tables.Count > 0)
+            var tablesKey = GetTablesInvolved(widgetModel);
+            DerivedAssociation derivedAssociation = null;
+
+            if (dashboard.TableAssociationHash.ContainsKey(tablesKey))
             {
+                derivedAssociation = dashboard.TableAssociationHash[tablesKey];
 
-                var allAssociations = GetAllTableAssociations();
-
-                var tablesConsidered = new List<string>();
-                tablesConsidered.Add(tables[0]);
-                query = db.Query(tables[0]);
-                for (var i = 0; i < tables.Count; i++)
+                if (null != derivedAssociation)
                 {
-                    var association = allAssociations.Where(a => a.TableName == tables[i]).FirstOrDefault();
-                    if(null != association)
+                    query = db.Query(derivedAssociation.TableName1);
+                    foreach (var rel in derivedAssociation.Relations)
                     {
-                        foreach (var rel in association.Relations)
-                        {
-                            if (tables.Contains(rel.TableName2) && !tablesConsidered.Contains(rel.TableName2))
-                            {
-                                //consider it
-                                query = query.Join(rel.TableName2, rel.Keys[0], rel.Keys[1], rel.Operation, rel.Type);
-                                tablesConsidered.Add(rel.TableName2);
-                            }
-                        }
+                        query = query.Join(rel.TableName2, rel.Keys[0], rel.Keys[1], rel.Operation, rel.Type);
                     }
-                   
                 }
-            }
-            else
+            } else
             {
-                if (widgetModel.Type == "filter" && widgetModel.FilterList.Count == 0)
+                if(tablesKey.Split(',').Length == 1)
                 {
-                    //Do not joing on any other table.
-                    //Select from table of dimention only
-                    var dimName = widgetModel.Dimension[0].Name;
-                    var tableName = dimName.Substring(0, dimName.IndexOf("."));
+                    var tableId = tablesKey.Split(',')[0];
+                    var tableName = dashboard.Tables.Where(t => t.Id.ToString() == tableId).First().Name;
                     query = db.Query(tableName);
                 }
-                //else
-                //{
-
-                //    int count = 0;
-
-                //    foreach (var a in allAssociations)
-                //    {
-                //        if (count == 0)
-                //        {
-                //            query = db.Query(a.TableName);
-                //        }
-
-                //        foreach (var relation in a.Relations)
-                //        {
-                //            query = query.Join(relation.TableName2, relation.Keys[0], relation.Keys[1], relation.Operation, relation.Type);
-                //        }
-
-                //        count++;
-                //    }
-                //}
             }
-            
+
+            //if (null != tables && tables.Count > 0)
+            //{
+            //    var allAssociations = dashboard.Associations; //GetAllTableAssociations();
+
+            //    var tablesConsidered = new List<string>();
+            //    tablesConsidered.Add(tables[0]);
+            //    query = db.Query(tables[0]);
+            //    for (var i = 0; i < tables.Count; i++)
+            //    {
+            //        var association = allAssociations.Where(a => a.TableName == tables[i]).FirstOrDefault();
+            //        if(null != association)
+            //        {
+            //            foreach (var rel in association.Relations)
+            //            {
+            //                if (tables.Contains(rel.TableName2) && !tablesConsidered.Contains(rel.TableName2))
+            //                {
+            //                    //consider it
+            //                    query = query.Join(rel.TableName2, rel.Keys[0], rel.Keys[1], rel.Operation, rel.Type);
+            //                    tablesConsidered.Add(rel.TableName2);
+            //                }
+            //            }
+            //        }
+
+            //    }
+            //}
+            //else
+            //{
+            //    if (widgetModel.Type == "filter" && widgetModel.FilterList.Count == 0)
+            //    {
+            //        //Do not joing on any other table.
+            //        //Select from table of dimention only
+            //        var dimName = widgetModel.Dimension[0].Name;
+            //        var tableName = dimName.Substring(0, dimName.IndexOf("."));
+            //        query = db.Query(tableName);
+            //    }                
+            //}
+
 
             return query;
         }
 
-        private List<string> GetTablesInvolved(WidgetModel widgetModel)
+        //private List<string> GetTablesInvolved(WidgetModel widgetModel)
+        private string GetTablesInvolved(WidgetModel widgetModel)
         {
             List<string> tables = new List<string>();
 
@@ -257,7 +394,28 @@ namespace DashboardApi.Controllers
                 }
             }
 
-            return tables;
+            //Get Table Objects and string for hash key
+            var tableList = new List<Table>();
+            var tableKey = string.Empty;
+            //tables = tables.Select(t=>t.).OrderBy
+            foreach ( var t in tables)
+            {
+               var table =  dashboard.Tables.Where(tbl => tbl.Name == t).FirstOrDefault();
+                if (null != table)
+                {
+                    tableList.Add(table);
+                    //
+                }
+            }
+            tableList = tableList.OrderBy(t => t.Id).ToList();
+            foreach (var t in tableList)
+            {
+                tableKey += t.Id + ",";
+            }
+                tableKey = tableKey.Substring(0, tableKey.LastIndexOf(","));
+
+            //return tables;
+            return tableKey;
         }
 
         // GET: api/Data
@@ -265,6 +423,8 @@ namespace DashboardApi.Controllers
         [HttpPost]
         public IEnumerable<dynamic> GetData(WidgetModel widgetModel)
         {
+            BuildTableMetatdata();
+
             IEnumerable<dynamic> data = null;
             Query query = null;
             var connection = new MySqlConnection(
@@ -366,20 +526,8 @@ namespace DashboardApi.Controllers
             }
 
             data = query.Get();
-
-            //var constraints = new Dictionary<string, object> {
-            //                    { "Year", 2017 },
-            //                    { "CategoryId", 198 },
-            //                    { "IsPublished", true }
-            //                };
-
-
-            //}
-
-            //SqlResult result = compiler.Compile(query);
-
-            //var users = new XQuery(connection, compiler).From("Users").Limit(10).Get();
-
+            
+            //query.comp
 
             return data;
         }
@@ -438,6 +586,7 @@ namespace DashboardApi.Controllers
 
         private static List<Association> GetAllTableAssociations()
         {
+            
             var associations = new List<Association>();
 
             var association = new Association();
@@ -458,11 +607,17 @@ namespace DashboardApi.Controllers
             rel2.Keys = keys2;
             rel2.Operation = "=";
             relations.Add(rel2);
+            rel2 = new Relation() { TableName2 = "PurchaseOrderDetail", Type = "left" };
+            keys2 = new List<string>();
+            keys2.Add("Product.ProductId");
+            keys2.Add("PurchaseOrderDetail.ProductId");
+            rel2.Keys = keys2;
+            rel2.Operation = "=";
+            relations.Add(rel2);
             association.Relations = relations;
             associations.Add(association);
 
             association = new Association();
-
             association.TableName = "ProductInventory";
             relations = new List<Relation>();
             rel = new Relation() { TableName2 = "Product", Type = "left" };
@@ -475,9 +630,90 @@ namespace DashboardApi.Controllers
             association.Relations = relations;
             associations.Add(association);
 
+            association = new Association();
+            association.TableName = "ProductVendor";
+            relations = new List<Relation>();
+            rel = new Relation() { TableName2 = "Product", Type = "left" };
+            keys = new List<string>();
+            keys.Add("Product.ProductId");
+            keys.Add("ProductVendor.ProductId");
+            rel.Keys = keys;
+            rel.Operation = "=";
+            relations.Add(rel);
+            association.Relations = relations;
+            associations.Add(association);
+
+            association = new Association();
+            association.TableName = "PurchaseOrderDetail";
+            relations = new List<Relation>();
+            rel = new Relation() { TableName2 = "PurchaseOrderHeader", Type = "left" };
+            keys = new List<string>();
+            keys.Add("PurchaseOrderDetail.PurchaseOrderId");
+            keys.Add("PurchaseOrderHeader.PurchaseOrderId");
+            rel.Keys = keys;
+            rel.Operation = "=";
+            relations.Add(rel);
+            association.Relations = relations;
+            associations.Add(association);
+
+            association = new Association();
+            association.TableName = "PurchaseOrderHeader";
+            relations = new List<Relation>();
+            rel = new Relation() { TableName2 = "PurchaseOrderDetail", Type = "left" };
+            keys = new List<string>();
+            keys.Add("PurchaseOrderDetail.PurchaseOrderId");
+            keys.Add("PurchaseOrderHeader.PurchaseOrderId");
+            rel.Keys = keys;
+            rel.Operation = "=";
+            relations.Add(rel);
+            association.Relations = relations;
+            associations.Add(association);
+
+
+
 
             return associations;
 
         }
+
+        public List<string> GetAllCombinations(Dashboard dashboard)
+        {
+            //var tables = new List<string>();
+            //tables.Add("0");
+            //tables.Add("1");
+            //tables.Add("2");
+            //tables.Add("3");
+
+
+            IList<string> list = dashboard.Tables.Select(t => t.Id.ToString()).ToList<string>(); ;
+            List<string> allCombinations = new List<String>();
+            for (int i = 1; i <= list.Count; i++)
+            {
+                var combis = new Facet.Combinatorics.Combinations<string>(
+                    list, i, Facet.Combinatorics.GenerateOption.WithoutRepetition);
+                allCombinations.AddRange(combis.Select(c => string.Join(",", c)));
+            }
+
+            foreach (var combi in allCombinations)
+                Console.WriteLine(combi);
+
+            return allCombinations;
+        }
+
+
+        private static int IsEdgeExist(int u, int v, List<Association> associations, Dashboard dashboard)
+        {
+            var value = 0;
+
+            if (associations.Where(a => a.TableName == dashboard.Tables[u].Name && a.Relations.Where(r => r.TableName2 == dashboard.Tables[v].Name).FirstOrDefault() != null ||
+                                        (a.TableName == dashboard.Tables[v].Name && a.Relations.Where(r => r.TableName2 == dashboard.Tables[u].Name).FirstOrDefault() != null)).FirstOrDefault() != null)
+            {
+                value = 1;
+            }
+
+
+            return value;
+        }
+
     }
 }
--- DashboardApi/Models/Association.cs
+++ DashboardApi/Models/Association.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace DashboardApi.Models
 {
     public class Relation
-    {       
+    {
         public string TableName2 { get; set; }
 
         public string Type { get; set; }
@@ -20,10 +20,66 @@ namespace DashboardApi.Models
 
     public class Association
     {
+        public Association()
+        {
+            this.Relations = new List<Relation>();
+        }
         public string TableName { get; set; }
 
         public List<Relation> Relations { get; set; }
     }
 
 
+    public class DerivedAssociation
+    {
+        public DerivedAssociation()
+        {
+            this.Relations = new List<Relation>();
+        }
+        public string TableName1 { get; set; }
+        public List<Relation> Relations { get; set; }
+    }
+
+
+    public class Table
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public int Order { get; set; }
+    }
+
+
+    public class Dashboard
+    {
+        private List<Table> _tables = new List<Table>();
+
+        public Dashboard()
+        {
+            _tables = new List<Table>();
+        }
+
+        public List<Table> Tables
+        {
+            get
+            {
+                return _tables;
+            }
+        }
+
+        public List<Association> Associations { get; set; }
+
+        public Dictionary<string, DerivedAssociation> TableAssociationHash { get; set; }
+
+        public void AddTable(Table table)
+        {
+            _tables.Add(table);
+
+            table.Id = _tables.Count() >= 1 ? _tables.Count() - 1 : 0;
+
+            table.Order = table.Id;
+
+        }
+    }
 }
